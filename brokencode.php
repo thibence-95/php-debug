c2ced8d0a7c615d136c4d6a9e0c1eaa9c68ee6a7
Thibence <salencethibaut95@gmail.com>
2020-02-07 11:41:35 +0100

    at exc 4

--- brokencode.php
+++ brokencode.php
@@ -39,7 +39,7 @@ new_exercise(3);
 // === Exercise 3 ===
 // This should echo ` "Debugged !" `, fix it so that that is the literal text echo'ed
 
-$str = “Debugged ! Also very fun”;
+$str = Debugged ! Also very fun”;
 echo substr($str, 0, 10);
 
 
